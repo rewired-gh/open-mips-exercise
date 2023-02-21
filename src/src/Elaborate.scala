5585e2d69a48a486b2ad4aa6cc847fdb44c64795
Takehana <cowpowermax@pm.me>
2023-02-21 16:15:39 +0800

    fix: MFC imports

--- src/src/Elaborate.scala
+++ src/src/Elaborate.scala
@@ -1,14 +1,16 @@
-import circt.stage._
+import chisel3.stage._
+import circt.stage.{CIRCTTarget, CIRCTTargetAnnotation}
+import circt.stage
 import mips.Cpu
 
 object Elaborate extends App {
-  val useMFC    = true // use MLIR-based firrtl compiler
+  val useMFC    = false // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
 
   def top = new Cpu()
 
   if (useMFC) {
-    (new ChiselStage)
+    (new stage.ChiselStage)
       .execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
   } else {
     (new ChiselStage).execute(args, generator)
